dc1a7a18c8db4b152121ea2850dc9ee9e93753cf
jeiron17 <113035739+jeiron17@users.noreply.github.com>
2023-08-25 11:40:40 -0500

    Add files via upload

---
+++ flaskr/App.py
@@ -0,0 +1,20 @@
+from flaskr import create_app
+from .modelos import db, Cancion, Album, Usuario, Medio
+from flask_restful import Api
+from .vistas import VistaCanciones, VistaCancion
+
+app = create_app('default')
+app_context = app.app_context()
+app_context.push()
+
+db.init_app(app)
+db.create_all()
+
+api = Api(app)
+api.add_resource(VistaCanciones,'/canciones')
+api.add_resource(VistaCancion,'/canciones/<int:id_cancion>')
+
+
+
+
+
